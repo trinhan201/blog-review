eedd3a65a0a36d34801f4b5296891298e087d312
trinhan201 <trinhphieuan@gmail.com>
2024-01-05 20:09:33 +0700

    commit #3

--- client/components/pageComp/author.js
+++ client/components/pageComp/author.js
@@ -225,7 +225,12 @@ const Author = () => {
                     </div>
                 </div>
             ) : (
-                <div className="h-screen"></div>
+                <div className="flex items-center justify-center h-screen">
+                    <div className="relative">
+                        <div className="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+                        <div className="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-[var(--primary-color)] animate-spin"></div>
+                    </div>
+                </div>
             )}
         </>
     );
